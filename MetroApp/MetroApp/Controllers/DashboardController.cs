4189301772e1211e6def091a0dd57ff68e9634c6
jenderson daniel antigua ortiz <jendersondanielantigua@gmail.com>
2024-04-11 02:44:21 -0400

    Se crearon lso controladores recarga y ver todas las tarjetas, se ajusto el controlador del dashboard, se creo el models recarga y se creo las vistas para ver todas las tarjetas y recargar, tambien se ajustaron unos cambios en la vista de index

--- MetroApp/MetroApp/Controllers/DashboardController.cs
+++ MetroApp/MetroApp/Controllers/DashboardController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace MetroApp.Controllers
@@ -11,6 +8,23 @@ namespace MetroApp.Controllers
         // GET: Dashboard
         public ActionResult Index()
         {
+            return View();
+        }
+
+        // GET: Dashboard/VerTodasLasTarjetas
+        public ActionResult VerTodasLasTarjetas()
+        {
+            // Simulamos una lista de tarjetas para mostrar en la vista
+            var tarjetas = new List<object>
+            {
+                new { Numero = "1234", Tipo = "MasterCard" },
+                new { Numero = "5678", Tipo = "Visa" }
+                // Aquí puedes agregar más tarjetas según sea necesario
+            };
+
+            // Pasamos la lista de tarjetas a la vista
+            ViewBag.Tarjetas = tarjetas;
+
             return View();
         }
     }
---
+++ MetroApp/MetroApp/Controllers/VerTodasLasTarjetasController.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MetroApp.Controllers
+{
+    public class VerTodasLasTarjetasController : Controller
+    {
+        // GET: VerTodasLasTarjetas
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: VerTodasLasTarjetas/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: VerTodasLasTarjetas/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: VerTodasLasTarjetas/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VerTodasLasTarjetas/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: VerTodasLasTarjetas/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: VerTodasLasTarjetas/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: VerTodasLasTarjetas/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
