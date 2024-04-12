ab6250256a5b02f5e746468d9f681f2268ea7c1b
jenderson daniel antigua ortiz <jendersondanielantigua@gmail.com>
2024-04-12 01:03:16 -0400

    Creacion recargar tarjeta

---
+++ MetroApp/MetroApp/Controllers/RecargaController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Web.Mvc;
+using MetroApp.DataLayer.Models;
+
+namespace MetroApp.Controllers
+{
+    public class RecargaController : Controller
+    {
+        public ActionResult Recargar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Recargar(Recarga recarga)
+        {
+            if (ModelState.IsValid)
+            {
+
+                return RedirectToAction("ConfirmacionRecarga");
+            }
+
+            return View(recarga);
+        }
+
+        public ActionResult ConfirmacionRecarga()
+        {
+            return View();
+        }
+    }
+}
